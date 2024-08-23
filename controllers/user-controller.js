903dbef178dbc07ac2facbe6f76b80ff398a250d
akonshina <arina.konshina@teskalabs.com>
2024-08-23 12:26:47 +0200

    add registration

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,46 @@
+const { prisma } = require('../prisma/prisma-client');
+const bcrypt = require('bcryptjs');
+const Jdenticon = require('jdenticon');
+const path = require('path');
+const fs = require('fs');
+
 const UserController = {
 	register: async (req, res) => {
-		res.send('register')
+		const { email, password, name } = req.body;
+
+		if (!email || !password || !name) {
+			return res.status(400).json({ error: 'All fields are required'});
+		}
+
+		try {
+			const existingUser = await prisma.user.findUnique(({ where: { email } }));
+
+			if (existingUser) {
+				return res.status(400).json({ error: 'User already exist' });
+			}
+
+			const hashedPassword = await bcrypt.hash(password, 10);
+
+			const png = Jdenticon.toPng(name, 200);
+			const avatarName = `${name}_${Date.now()}.png`;
+			const avatarPath = path.join(__dirname, '../uploads', avatarName);
+			fs.writeFileSync(avatarPath, png);
+
+			const user = await prisma.user.create({
+				data: {
+					email,
+					password: hashedPassword,
+					name,
+					avatarUrl: `/uploads/${avatarPath}`
+				}
+			})
+
+			res.json(user);
+
+		} catch (e) {
+			console.error('Error in register', e);
+			res.status(500).json({ error: 'Internal server error '});
+		}
 	},
 	login: async (req, res) => {
 		res.send('login')
@@ -13,7 +53,7 @@ const UserController = {
 	},
 	current: async (req, res) => {
 		res.send('current')
-	},
+	}
 };
 
 module.exports = UserController;
