86a2f4909185b1bbdcd72216ce0ebd6ab2d92e5f
akonshina <arina.konshina@teskalabs.com>
2024-08-26 10:20:40 +0200

    create all post controller

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -25,13 +25,89 @@ const PostController = {
 		}
 	},
 	getAllPosts: async (req, res) => {
-		res.send('getAllPosts')
+		const userId = req.user.userId;
+
+		try {
+			const posts = await prisma.post.findMany({
+				include: {
+					likes: true,
+					author: true,
+					comments: true,
+				},
+				orderBy: {
+					createdAt: 'desc'
+				},
+			});
+
+			const postWithLikeInfo = posts.map(post => ({
+				...post,
+				likedByUser: post.likes.some(like => like.userId === userId)
+			}));
+
+			res.json(postWithLikeInfo);
+		} catch (e) {
+			console.error('Get all post error', e);
+			res.status(500).json({ error: 'Internal server error' });
+		}
 	},
 	getPostById: async (req, res) => {
-		res.send('getPostById')
+		const { id } =  req.params;
+		const userId = req.user.userId;
+
+		try {
+			const post = await prisma.post.findUnique({
+				where: { id },
+				include: {
+					comments: {
+						include: {
+							user: true,
+						},
+					},
+					likes: true,
+					author: true,
+				},
+			});
+
+			if (!post) {
+				res.status(404).json({ error: 'The post was not found' });
+			}
+
+			const postWithLikeInfo = {
+				...post,
+				likedByUser: post.likes.some(like => like.userId === userId),
+			};
+
+			res.json(postWithLikeInfo);
+		} catch (e) {
+			console.error('Get post by Id error', e);
+			res.status(500).json({ error: 'Internal server error' });
+		}
 	},
 	deletePost: async (req, res) => {
-		res.send('deletePost')
+		const { id } =  req.params;
+
+		const post = await prisma.post.findUnique({where: { id }});
+
+		if (!post) {
+			res.status(404).json({ error: 'The post was not found' });
+		}
+
+		if (post.authorId !== req.user.userId) {
+			return res.status(403).json({ error: 'Not access' });
+		}
+
+		try {
+			const transaction = await prisma.$transaction([
+				prisma.comment.deleteMany({ where: { postId: id } }),
+				prisma.like.deleteMany({ where: { postId: id } }),
+				prisma.post.deleteMany({ where: { id } }),
+			]);
+
+			res.json(transaction);
+		} catch (e) {
+			console.error('Delete post error', e);
+			res.status(500).json({ error: 'Internal server error' });
+		}
 	},
 }
 
