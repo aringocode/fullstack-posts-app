e8e29bea8d5323272cf97f399912584402e37ab3
akonshina <arina.konshina@teskalabs.com>
2024-08-26 09:56:05 +0200

    createPost

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -2,7 +2,27 @@ const { prisma } = require('../prisma/prisma-client');
 
 const PostController = {
 	createPost: async (req, res) => {
-		res.send('createPost')
+		const { content } = req.body;
+
+		const authorId = req.user.userId;
+
+		if (!content) {
+			return res.status(400).json({ error: 'All fields are required' });
+		}
+
+		try {
+			const post = await prisma.post.create({
+				data: {
+					content,
+					authorId,
+				}
+			});
+
+			res.json(post);
+		} catch (e) {
+			console.error('Create post error', e);
+			res.status(500).json({ error: 'Internal server error' });
+		}
 	},
 	getAllPosts: async (req, res) => {
 		res.send('getAllPosts')
